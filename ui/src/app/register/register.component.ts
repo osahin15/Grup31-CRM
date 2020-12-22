6940f2af13525119ee9bbf0f52a51374414d1b3a
osahin45 <37152719+osahin45@users.noreply.github.com>
2020-12-22 15:28:10 +0300

    Login yapıldı.

--- ui/src/app/register/register.component.ts
+++ ui/src/app/register/register.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../login/user';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [AuthService]
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  register(user: User) {
+    this.authService.register(user)
+  }
 }
